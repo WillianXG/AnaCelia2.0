4ae374f2a48c241eae39fc06a94a5ace9f58158e
WillianXG <wilian.mine789@gmail.com>
2024-07-08 20:57:37 -0300

    pages

--- app.vue
+++ app.vue
@@ -1,14 +1,52 @@
 <template>
-  <NuxtLayout>
-    <v-app>
-      <v-navigation-drawer>
-        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
-        <v-divider></v-divider>
-        <v-list-item link title="List Item 1"></v-list-item>
-        <v-list-item link title="List Item 2"></v-list-item>
-        <v-list-item link title="List Item 3"></v-list-item>
-      </v-navigation-drawer>
-      <NuxtPage />
-    </v-app>
-  </NuxtLayout>
+  <v-app>
+    <v-app-bar app color="#000000" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ana Celia Psicologa</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+          </v-list-item-icon >
+          <v-list-item-title>Início</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Sobre</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/treatment">
+            <v-icon>mdi-settings</v-icon>
+          <v-list-item-title>Tratamentos</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <!-- Conteúdo principal do seu aplicativo aqui -->
+      <div class="text-center mt-15">
+        <NuxtPage/>
+      </div>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+
+<style>
+/* Estilo para ocultar a barra de rolagem vertical */
+.v-application {
+  overflow-y: hidden;
+}
+</style>
